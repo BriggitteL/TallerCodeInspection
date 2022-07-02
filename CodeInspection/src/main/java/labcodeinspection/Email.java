39f31c7466a0750aae25444938cf91adc222f0ee
Briggitte Lizbeth Lopez Vaca <33633208+BriggitteL@users.noreply.github.com>
2022-07-02 18:05:03 -0500

    Codigo de Taller CodeInspection corregido

---
+++ CodeInspection/src/main/java/labcodeinspection/Email.java
@@ -0,0 +1,167 @@
+package labcodeinspection;
+
+import java.util.Locale;
+
+public class Email {
+
+	private String m_firstName; 
+	private String m_lastName;
+	private String password;
+	private String department;
+	private int defaultpasswordLength = 8;
+	private String email;
+
+	/**
+	*
+	*
+	* @method Email
+	*/
+	public Email(String firstName, String lastName) {
+		this.m_firstName = firstName;
+		this.m_lastName = lastName;
+	}	
+
+	/**
+	*
+	*
+	* @method getM_firstName
+	*/
+	public String getM_firstName() {
+		return m_firstName;
+	}
+
+
+	/**
+	*
+	*
+	* @method setM_firstName
+	*/
+	public void setM_firstName(String m_firstName) {
+		this.m_firstName = m_firstName;
+	}
+
+
+	/**
+	*
+	*
+	* @method getM_lastName
+	*/
+	public String getM_lastName() {
+		return m_lastName;
+	}
+
+
+	/**
+	*
+	*
+	* @method setM_lastName
+	*/
+	public void setM_lastName(String m_lastName) {
+		this.m_lastName = m_lastName;
+	}
+
+
+	
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getDepartment() {
+		return department;
+	}
+
+
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+
+
+	public int getDefaultpasswordLength() {
+		return defaultpasswordLength;
+	}
+
+
+
+	public void setDefaultpasswordLength(int defaultpasswordLength) {
+		this.defaultpasswordLength = defaultpasswordLength;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	/**
+	*
+	*
+	* @method showInfo
+	*/
+	public void showInfo() {
+		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
+		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+	}
+	
+	
+	/**
+	*
+	*
+	* @method setDeparment
+	*/
+	public void setDeparment(int depChoice) {
+		switch (depChoice) {
+		case 1:
+			this.department = "sales";
+			break;
+		case 2:
+			this.department = "dev";
+			break;
+		case 3:
+			this.department = "acct";
+			break;
+		default:
+			this.department = "";
+			break;
+		}
+		
+			
+	}
+
+	private String randomPassword(int length) {
+		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+		char[] password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * set.length());
+			password[i] = set.charAt(rand);
+		}
+		return new String(password);
+	}
+
+	/**
+	*
+	*
+	* @method generateEmail
+	*/
+	public void generateEmail() {
+		this.password = this.randomPassword(this.defaultpasswordLength);
+		this.email = this.m_firstName.toLowerCase(Locale.getDefault()) + this.m_lastName.toLowerCase(Locale.getDefault()) + "@" + this.department
+				+ ".espol.edu.ec";
+	}
+}
